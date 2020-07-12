3c220c2f21370df78f4389f0c1ee258f323c789e
scottxu <xyc0714@aliyun.com>
2020-07-12 19:00:22 +0800

    Update Socket.h

--- rtsp-server/net/Socket.h
+++ rtsp-server/net/Socket.h
@@ -4,28 +4,7 @@
 #ifndef XOP_SOCKET_H
 #define XOP_SOCKET_H
 
-#if defined(__unix) || defined(__unix__) 
-#include <sys/types.h>         
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h> 
-#include <netinet/ether.h>   
-#include <netinet/ip.h>  
-#include <netpacket/packet.h>   
-#include <arpa/inet.h>
-#include <net/ethernet.h>   
-#include <net/route.h>  
-#include <netdb.h>
-#include <net/if.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/select.h>
-#define SOCKET int
-#define INVALID_SOCKET  (-1)
-#define SOCKET_ERROR    (-1) 
-
-#elif defined(WIN32) || defined(_WIN32) 
+#if defined(WIN32) || defined(_WIN32)
 #define FD_SETSIZE      1024
 #define WIN32_LEAN_AND_MEAN
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
@@ -38,6 +17,25 @@
 #define SHUT_RDWR 2
 
 #else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <netpacket/packet.h>
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <net/route.h>
+#include <netdb.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/select.h>
+#define SOCKET int
+#define INVALID_SOCKET (-1)
+#define SOCKET_ERROR (-1)
 
 #endif
 
