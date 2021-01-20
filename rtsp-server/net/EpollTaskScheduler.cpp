bbdf7fe8913d04045f2cf860029074c9d472a9fb
scottxu <scottxu@scottxublog.com>
2021-01-20 17:35:33 +0800

    Added MacOS support.

--- rtsp-server/net/EpollTaskScheduler.cpp
+++ rtsp-server/net/EpollTaskScheduler.cpp
@@ -1,103 +1,175 @@
 // PHZ
 // 2018-5-15
+// Scott Xu
+// 2021-1-19 Added MacOS support.
 
 #include "EpollTaskScheduler.h"
 
-#if defined(__linux) || defined(__linux__) 
+#if defined(__linux) || defined(__linux__)
 #include <sys/epoll.h>
+#include <errno.h>
+#elif defined(__APPLE__) || defined(__MACH__)
+#include <sys/event.h>
 #include <errno.h>
 #endif
 
 using namespace xop;
 
-EpollTaskScheduler::EpollTaskScheduler(int id)
-	: TaskScheduler(id)
+EpollTaskScheduler::EpollTaskScheduler(int id) : TaskScheduler(id)
 {
-#if defined(__linux) || defined(__linux__) 
-    epollfd_ = epoll_create1(0);
- #endif
-    this->UpdateChannel(wakeup_channel_);
+#if defined(__linux) || defined(__linux__)
+	epollfd_ = epoll_create1(0);
+#elif defined(__APPLE__) || defined(__MACH__)
+        epollfd_ = kqueue();
+#endif
+	this->UpdateChannel(wakeup_channel_);
 }
 
-EpollTaskScheduler::~EpollTaskScheduler()
-{
-	
-}
+EpollTaskScheduler::~EpollTaskScheduler() {}
 
 void EpollTaskScheduler::UpdateChannel(ChannelPtr channel)
 {
 	std::lock_guard<std::mutex> lock(mutex_);
-#if defined(__linux) || defined(__linux__) 
+#if defined(__linux) || defined(__linux__)
 	int fd = channel->GetSocket();
-	if(channels_.find(fd) != channels_.end()) {
-		if(channel->IsNoneEvent()) {
+	if (channels_.find(fd) != channels_.end()) {
+		if (channel->IsNoneEvent()) {
 			Update(EPOLL_CTL_DEL, channel);
 			channels_.erase(fd);
-		}
-		else {
+		} else {
 			Update(EPOLL_CTL_MOD, channel);
 		}
-	}
-	else {
-		if(!channel->IsNoneEvent()) {
+	} else {
+		if (!channel->IsNoneEvent()) {
 			channels_.emplace(fd, channel);
 			Update(EPOLL_CTL_ADD, channel);
-		}	
-	}	
+		}
+	}
+#elif defined(__APPLE__) || defined(__MACH__)
+        int fd = channel->GetSocket();
+        if (channels_.find(fd) != channels_.end()) {
+                if (channel->IsNoneEvent()) {
+                        Update(EV_DELETE, channel);
+                        channels_.erase(fd);
+                } else {
+                        Update(EV_ADD|EV_ENABLE, channel);
+                }
+        } else {
+                if (!channel->IsNoneEvent()) {
+                        channels_.emplace(fd, channel);
+                        Update(EV_ADD|EV_ENABLE, channel);
+                }
+        }
 #endif
 }
 
-void EpollTaskScheduler::Update(int operation, ChannelPtr& channel)
+void EpollTaskScheduler::Update(int operation, ChannelPtr &channel)
 {
-#if defined(__linux) || defined(__linux__) 
+#if defined(__linux) || defined(__linux__)
 	struct epoll_event event = {0};
 
-	if(operation != EPOLL_CTL_DEL) {
+	if (operation != EPOLL_CTL_DEL) {
 		event.data.ptr = channel.get();
 		event.events = channel->GetEvents();
 	}
 
-	if(::epoll_ctl(epollfd_, operation, channel->GetSocket(), &event) < 0) {
-
+	if (::epoll_ctl(epollfd_, operation, channel->GetSocket(), &event) <
+	    0) {
 	}
+#elif defined(__APPLE__) || defined(__MACH__)
+        struct kevent events[2] = {0};
+        int num_events = 0;
+
+	if (channel->IsReading())
+		EV_SET(&events[num_events++], channel->GetSocket(), EVFILT_READ, operation, NULL, NULL, channel.get());
+        if (channel->IsWriting())
+                EV_SET(&events[num_events++], channel->GetSocket(), EVFILT_WRITE, operation, NULL, NULL, channel.get());
+
+        if (kevent(epollfd_, events, num_events, nullptr, 0, nullptr) <
+            0) {
+        }
 #endif
 }
 
-void EpollTaskScheduler::RemoveChannel(ChannelPtr& channel)
+void EpollTaskScheduler::RemoveChannel(ChannelPtr &channel)
 {
-    std::lock_guard<std::mutex> lock(mutex_);
-#if defined(__linux) || defined(__linux__) 
+	std::lock_guard<std::mutex> lock(mutex_);
+#if defined(__linux) || defined(__linux__)
 	int fd = channel->GetSocket();
 
-	if(channels_.find(fd) != channels_.end()) {
+	if (channels_.find(fd) != channels_.end()) {
 		Update(EPOLL_CTL_DEL, channel);
 		channels_.erase(fd);
 	}
+#elif defined(__APPLE__) || defined(__MACH__)
+        int fd = channel->GetSocket();
+
+        if (channels_.find(fd) != channels_.end()) {
+                Update(EV_DELETE, channel);
+                channels_.erase(fd);
+        }
 #endif
 }
 
 bool EpollTaskScheduler::HandleEvent(int timeout)
 {
-#if defined(__linux) || defined(__linux__) 
+#if defined(__linux) || defined(__linux__)
 	struct epoll_event events[512] = {0};
 	int num_events = -1;
 
 	num_events = epoll_wait(epollfd_, events, 512, timeout);
-	if(num_events < 0)  {
-		if(errno != EINTR) {
+	if (num_events < 0) {
+		if (errno != EINTR) {
 			return false;
-		}								
+		}
 	}
 
-	for(int n=0; n<num_events; n++) {
-		if(events[n].data.ptr) {        
-			((Channel *)events[n].data.ptr)->HandleEvent(events[n].events);
+	for (int n = 0; n < num_events; n++) {
+		if (events[n].data.ptr) {
+			((Channel *)events[n].data.ptr)
+				->HandleEvent(events[n].events);
 		}
-	}		
+	}
 	return true;
+#elif defined(__APPLE__) || defined(__MACH__)
+        struct kevent events[512] = {0};
+        int num_events = -1;
+
+	if (timeout > 0) {
+                struct timespec _timeout = {0};
+                _timeout.tv_sec = timeout / 1000;
+                _timeout.tv_nsec = (timeout % 1000) * 1000 * 1000;
+                num_events = kevent(epollfd_, nullptr, 0, events, 512, &_timeout);
+	} else
+                num_events = kevent(epollfd_, nullptr, 0, events, 512, nullptr);
+        if (num_events < 0) {
+                if (errno != EINTR) {
+                        return false;
+                }
+        }
+
+        for (int n = 0; n < num_events; n++) {
+                auto filter = events[n].filter;
+                auto flags = events[n].flags;
+                auto channel = (Channel *)events[n].udata;
+
+                if (!channel) continue;
+
+		int handleEventEvents = EVENT_NONE;
+		if (filter == EVFILT_READ)
+                        handleEventEvents = EVENT_IN;
+		else if (filter == EVFILT_WRITE)
+			handleEventEvents = EVENT_OUT;
+
+		if (flags & EV_ERROR)
+                        handleEventEvents = handleEventEvents | EVENT_ERR;
+		if (flags & EV_EOF)
+                        handleEventEvents = handleEventEvents | EVENT_HUP;
+
+                channel->HandleEvent(handleEventEvents);
+        }
+        return true;
 #else
-    return false;
+	return false;
 #endif
 }
-
-
--- rtsp-server/xop/DigestAuthentication.cpp
+++ rtsp-server/xop/DigestAuthentication.cpp
@@ -1,5 +1,47 @@
 #include "DigestAuthentication.h"
-#include "md5/md5.hpp" 
+#if defined(__APPLE__) || defined(__MACH__)
+#include <chrono>
+#include <random>
+#include <CommonCrypto/CommonDigest.h>
+#else
+#include "md5/md5.hpp"
+#endif
+
+#if defined(__APPLE__) || defined(__MACH__)
+std::string md5_hash_hex(std::string const & input)
+{
+        unsigned char result[CC_MD5_DIGEST_LENGTH];
+        const char hexValue[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        CC_MD5(input.c_str(), (CC_LONG)input.length(), result);
+        char md5Hex[2 * CC_MD5_DIGEST_LENGTH + 1];
+        for (size_t i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+                md5Hex[2 * i] = hexValue[((result[i] >> 4) & 0xF)];
+                md5Hex[2 * i + 1] = hexValue[(result[i]) & 0x0F];
+        }
+        md5Hex[2 * CC_MD5_DIGEST_LENGTH] = '\0';
+        return md5Hex;
+}
+
+std::string generate_nonce()
+{
+        std::random_device rd;
+
+        auto timePoint = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now());
+        uint32_t timestamp = (uint32_t)timePoint.time_since_epoch().count();
+
+        md5_hash_hex(std::to_string(timestamp + rd()));
+}
+#else
+std::string md5_hash_hex(std::string const & input)
+{
+	return md5::md5_hash_hex(input);
+}
+
+std::string generate_nonce()
+{
+	return md5::generate_nonce();
+}
+#endif
 
 using namespace xop;
 
@@ -18,16 +60,15 @@ DigestAuthentication::~DigestAuthentication()
 
 std::string DigestAuthentication::GetNonce()
 {
-	return md5::generate_nonce();
+	return generate_nonce();
 }
 
 std::string DigestAuthentication::GetResponse(std::string nonce, std::string cmd, std::string url)
 {
 	//md5(md5(<username>:<realm> : <password>) :<nonce> : md5(<cmd>:<url>))
-
-	auto hex1 = md5::md5_hash_hex(m_username + ":" + m_realm + ":" + m_password);
-	auto hex2 = md5::md5_hash_hex(cmd + ":" + url);
-	auto response = md5::md5_hash_hex(hex1 + ":" + nonce + ":" + hex2);
+	auto hex1 = md5_hash_hex(m_username + ":" + m_realm + ":" + m_password);
+	auto hex2 = md5_hash_hex(cmd + ":" + url);
+	auto response = md5_hash_hex(hex1 + ":" + nonce + ":" + hex2);
 	return response;
 }
 
