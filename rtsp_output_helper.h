046ad14cc753ec27c181668d6559843315c5abaa
scottxu <scottxu@scottxublog.com>
2021-02-21 21:24:23 +0800

    Fix errors.

--- rtsp_output_helper.h
+++ rtsp_output_helper.h
@@ -14,13 +14,13 @@ class RtspOutputHelper {
 public:
 	RtspOutputHelper(std::string outputName);
 	~RtspOutputHelper();
-	static RtspOutputHelper *RtspOutputHelper::CreateRtspOutput(obs_data_t *settings, obs_data_t *hotkey);
+	static RtspOutputHelper *CreateRtspOutput(obs_data_t *settings, obs_data_t *hotkey);
 	void UpdateSettings(obs_data_t *settings);
 	void UpdateEncoder();
 	bool Start();
 	void Stop();
 	std::string GetLastError();
-	obs_data_t *RtspOutputHelper::HotkeysSave();
+	obs_data_t *HotkeysSave();
 	void SignalConnect(const char *signal, signal_callback_t callback,
 			   void *data);
 	void SignalDisconnect(const char *signal, signal_callback_t callback,
@@ -33,7 +33,7 @@ private:
 	void CreateVideoEncoder();
 	void CreateAudioEncoder();
 	void GetBaseConfig();
-	static void RtspOutputHelper::OnPreStartSignal(void *data, calldata_t *cd);
+	static void OnPreStartSignal(void *data, calldata_t *cd);
 	obs_output_t *obsOutput;
 	struct rtsp_output_settings outputSettings;
 	obs_encoder_t *videoEncoder = nullptr;
