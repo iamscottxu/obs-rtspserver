6ae2b05fbca4bdf454e6841a16b0b1755f81f29c
scottxu <scottxu@scottxublog.com>
2022-09-18 00:09:46 +0800

    build(linux): Updated build scripts to fit OBS 28.
    Signed-off-by: scottxu <scottxu@scottxublog.com>

--- CI/linux/01_install_dependencies.sh
+++ CI/linux/01_install_dependencies.sh
@@ -83,7 +83,8 @@ install_dependencies() {
          libswscale-dev libx264-dev libcurl4-openssl-dev libmbedtls-dev libgl1-mesa-dev libjansson-dev \
          libluajit-5.1-dev python3-dev libx11-dev libxcb-randr0-dev libxcb-shm0-dev libxcb-xinerama0-dev \
          libxcb-composite0-dev libxinerama-dev libxcb1-dev libx11-xcb-dev libxcb-xfixes0-dev swig libcmocka-dev \
-         libpci-dev libxss-dev libglvnd-dev libgles2-mesa libgles2-mesa-dev libwayland-dev libxkbcommon-dev"
+         libpci-dev libxss-dev libglvnd-dev libgles2-mesa libgles2-mesa-dev libwayland-dev libxkbcommon-dev \
+         libpulse-dev"
         "qt5-deps qtbase5-dev qtbase5-private-dev libqt5svg5-dev qtwayland5"
         "qt6-deps qt6-base-dev qt6-base-private-dev libqt6svg6-dev qt6-wayland"
     )
