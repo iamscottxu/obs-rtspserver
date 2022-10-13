770ee82ffe037b375a6de1d9143e9a66dcbb8e90
scottxu <scottxu@scottxublog.com>
2022-10-13 23:23:23 +0800

    fix(rtsp-server): Fixed code error calling function "epoll_create1()".

--- rtsp-server/net/EpollTaskScheduler.cpp
+++ rtsp-server/net/EpollTaskScheduler.cpp
@@ -13,7 +13,7 @@ using namespace xop;
 EpollTaskScheduler::EpollTaskScheduler(const int id) : TaskScheduler(id)
 {
 #if defined(__linux) || defined(__linux__)
-	epollfd_ = epoll_create1(1024);
+	epollfd_ = epoll_create1(0);
 #endif
 	this->EpollTaskScheduler::UpdateChannel(wakeup_channel_);
 }
